a29ebf5d37df2587d28d2f6bba4eac5942e541ae
Александр <smirnov.togar@ya.ru>
2019-12-18 12:02:26 +0300

    добавил 2 трек только в интерфейсе

--- Main_project/user/main.c
+++ Main_project/user/main.c
@@ -5,17 +5,16 @@
 #include "MDR32F9Qx_rst_clk.h" //библиотека для тактирования
 #include "MDR32F9Qx_adc.h"	 //библиотека для работы с АЦП
 #include "MDR32F9Qx_dac.h"	 //библиотека для работы с ЦАП
-#include "MDR32F9Qx_timer.h"    // библиотека для работы с таймером
-
+#include "MDR32F9Qx_timer.h"   // библиотека для работы с таймером
 
 #define EEPROM_PAGE_SIZE (4 * 1024) //1 страница памяти - 4К
 #define MAIN_EEPAGE 5
 #define ENTIRE_MEM_BYTES 110592 // свободная память до 0x08020000
-#define CPU_FREQ 20000000 //20МГц
+#define CPU_FREQ 20000000		//20МГц
 
-int global_byte_counter = 4; //начиная с 4 бита 
+int global_byte_counter = 4;		//начиная с 4 бита
 uint8_t global_adc_result = 0x0800; //среднее значение
-uint16_t global_dac_result = 0x80; //ср	еднее значение
+uint16_t global_dac_result = 0x80;  //ср	еднее значение
 uint32_t global_max = 0x0000;
 uint32_t global_min = 0xFFFF;
 
@@ -29,66 +28,69 @@ void Delay(int num)
 }
 
 /* Функция преобразования частоты дискретизации в число тактов отсчета таймера */
-int freq_to_tact(int freq_dis) {
-	return (int)((1/(double)freq_dis)/(1/(double)CPU_FREQ));
+int freq_to_tact(int freq_dis)
+{
+	return (int)((1 / (double)freq_dis) / (1 / (double)CPU_FREQ));
 }
 
 /* Процедура настройки таймера 1 */
-void Timer1_init(int freq) {
+void Timer1_init(int freq)
+{
 	int period = freq_to_tact(freq);
 	//Включение тактирования
 	RST_CLK_PCLKcmd(RST_CLK_PCLK_TIMER1, ENABLE);
 
 	//Инициализация структур
 	TIMER_CntInitTypeDef timerCnt;
-	
+
 	//Установка настроек по умолчанию
 	TIMER_CntStructInit(&timerCnt);
 
 	//Установка предделителя
 	TIMER_BRGInit(MDR_TIMER1, TIMER_HCLKdiv1);
-	
+
 	//Установка числа отсчетов до прерывания
 	timerCnt.TIMER_Period = period;
 
-	//Применение настроек	
+	//Применение настроек
 	TIMER_CntInit(MDR_TIMER1, &timerCnt);
 	//Установка наибольшего приоритета
-	NVIC_SetPriority (Timer1_IRQn, 0);
+	NVIC_SetPriority(Timer1_IRQn, 0);
 	//Установка прерывания по окончанию счета таймера (достижения 0)
 	TIMER_ITConfig(MDR_TIMER1, TIMER_STATUS_CNT_ZERO, ENABLE);
 	//Разрешение прерываний
 	__enable_irq();
 	//Разрешение прерываний
-	NVIC_EnableIRQ (Timer1_IRQn);
+	NVIC_EnableIRQ(Timer1_IRQn);
 	NVIC_ClearPendingIRQ(Timer1_IRQn);
 }
 
-
 /* Обработчик прерывания таймера 1 */
-void Timer1_IRQHandler(){
+void Timer1_IRQHandler()
+{
 	uint16_t Data;
 	uint32_t Address = 0;
 	uint32_t BankSelector = 0;
 	Address = 0x08000000 + EEPROM_PAGE_SIZE * MAIN_EEPAGE;
 	BankSelector = EEPROM_Main_Bank_Select;
 
-	if (TIMER_GetITStatus(MDR_TIMER1, TIMER_STATUS_CNT_ZERO)){
+	if (TIMER_GetITStatus(MDR_TIMER1, TIMER_STATUS_CNT_ZERO))
+	{
 		TIMER_ClearITPendingBit(MDR_TIMER1, TIMER_STATUS_CNT_ZERO);
 		/* Начало логики обработки прерывания*/
 		//Запуск преобразования и получение нового результата
 		Data = ADC_Receive_Word();
-	    //отбрасывание младших 4 разрядов
-	    global_adc_result = (uint8_t)(Data >> 4);
+		//отбрасывание младших 4 разрядов
+		global_adc_result = (uint8_t)(Data >> 4);
 
-	    //запись очередного значения в память (1514 тактов)
-	    EEPROM_ProgramByte(Address + global_byte_counter, BankSelector, global_adc_result);
-
+		//запись очередного значения в память (1514 тактов)
+		EEPROM_ProgramByte(Address + global_byte_counter, BankSelector, global_adc_result);
 
 		//Инкремент счетчика бит
 		global_byte_counter += 1;
 		//Остановка таймера, если счетчик выходит на пределы доступной памяти
-		if (global_byte_counter > 110588) {
+		if (global_byte_counter > 110588)
+		{
 			TIMER_Cmd(MDR_TIMER1, DISABLE);
 		}
 		/* Конец логики обработки прерывания*/
@@ -96,39 +98,41 @@ void Timer1_IRQHandler(){
 }
 
 /* Процедура настройки таймера 2 */
-void Timer2_init(int freq) {
-	
+void Timer2_init(int freq)
+{
+
 	int period = freq_to_tact(freq);
 	//Включение тактирования
 	RST_CLK_PCLKcmd(RST_CLK_PCLK_TIMER2, ENABLE);
 
 	//Инициализация структур
 	TIMER_CntInitTypeDef timerCnt;
-	
+
 	//Установка настроек по умолчанию
 	TIMER_CntStructInit(&timerCnt);
 
 	//Установка предделителя
 	TIMER_BRGInit(MDR_TIMER2, TIMER_HCLKdiv1);
-	
+
 	//Установка числа отсчетов до прерывания
 	timerCnt.TIMER_Period = period;
 
-	//Применение настроек	
+	//Применение настроек
 	TIMER_CntInit(MDR_TIMER2, &timerCnt);
 	//Установка наибольшего приоритета
-	NVIC_SetPriority (Timer2_IRQn, 0);
+	NVIC_SetPriority(Timer2_IRQn, 0);
 	//Установка прерывания по окончанию счета таймера (достижения 0)
 	TIMER_ITConfig(MDR_TIMER2, TIMER_STATUS_CNT_ZERO, ENABLE);
 	//Разрешение прерываний
 	__enable_irq();
 	//Разрешение прерываний
-	NVIC_EnableIRQ (Timer2_IRQn);
+	NVIC_EnableIRQ(Timer2_IRQn);
 	NVIC_ClearPendingIRQ(Timer2_IRQn);
 }
 
 /* Обработчик прерывания таймера 2 */
-void Timer2_IRQHandler(){
+void Timer2_IRQHandler()
+{
 	uint16_t Data;
 	uint8_t Data_8bit;
 	uint32_t Address = 0;
@@ -136,21 +140,23 @@ void Timer2_IRQHandler(){
 	Address = 0x08000000 + EEPROM_PAGE_SIZE * MAIN_EEPAGE;
 	BankSelector = EEPROM_Main_Bank_Select;
 
-	if (TIMER_GetITStatus(MDR_TIMER2, TIMER_STATUS_CNT_ZERO)){
+	if (TIMER_GetITStatus(MDR_TIMER2, TIMER_STATUS_CNT_ZERO))
+	{
 		TIMER_ClearITPendingBit(MDR_TIMER2, TIMER_STATUS_CNT_ZERO);
 		/* Начало логики обработки прерывания*/
 		//Вывод предыдущего значения на ЦАП
 		DAC2_SetData(global_dac_result);
 		//Чтение очередного значения
 		Data_8bit = (EEPROM_ReadByte(Address + global_byte_counter, BankSelector));
-	    Data = (((uint16_t)Data_8bit) << 4) + 0x7;
+		Data = (((uint16_t)Data_8bit) << 4) + 0x7;
 		//Нормализация
 		//Data = ((uint16_t)(((double)(Data - global_min))*(((double)0xFFF) / ((double)(global_max - global_min)))) + 0x800) & 0xFFF;
 		global_dac_result = Data;
 		//Инкремент счетчика бит
 		global_byte_counter += 1;
 		//Остановка таймера, если счетчик выходит на пределы доступной памяти
-		if (global_byte_counter > 110588) {
+		if (global_byte_counter > 110588)
+		{
 			TIMER_Cmd(MDR_TIMER2, DISABLE);
 		}
 		/* Конец логики обработки прерывания*/
@@ -162,7 +168,8 @@ void MY_U_RST_Init(void)
 {
 	RST_CLK_PCLKcmd(RST_CLK_PCLK_BKP, ENABLE);
 	RST_CLK_HSEconfig(RST_CLK_HSE_ON);
-	while (RST_CLK_HSEstatus() != SUCCESS);
+	while (RST_CLK_HSEstatus() != SUCCESS)
+		;
 
 	//20 Мгц
 	RST_CLK_CPU_PLLconfig(RST_CLK_CPU_PLLsrcHSEdiv2,
@@ -185,8 +192,9 @@ uint16_t ADC_Receive_Word()
 {
 	uint16_t result;
 	ADC1_Start();
-	while (ADC1_GetFlagStatus(ADC1_FLAG_END_OF_CONVERSION) == 0);
-	result = ADC1_GetResult() & 0x00000FFF;//получение 12-разрядного результата
+	while (ADC1_GetFlagStatus(ADC1_FLAG_END_OF_CONVERSION) == 0)
+		;
+	result = ADC1_GetResult() & 0x00000FFF; //получение 12-разрядного результата
 	return result;
 }
 
@@ -221,7 +229,9 @@ void write_track()
 	//Запуск таймера
 	TIMER_Cmd(MDR_TIMER1, ENABLE);
 	//ожидание окончания записи
-	while (global_byte_counter <= 110588){}
+	while (global_byte_counter <= 110588)
+	{
+	}
 	global_byte_counter = 4;
 	//создание метки
 	EEPROM_ProgramWord(Address, BankSelector, 0xABCDEFAB);
@@ -257,7 +267,9 @@ void read_track()
 	//Запуск таймера
 	TIMER_Cmd(MDR_TIMER2, ENABLE);
 	//Ожидание завершения воспроизведения
-	while (global_byte_counter <= 110588){}
+	while (global_byte_counter <= 110588)
+	{
+	}
 	global_byte_counter = 4;
 }
 
@@ -321,8 +333,10 @@ void MY_DAC2_Init(void)
 
 /* Функция определения статуа (нажата или нет) кнопок (на пульте оператора)*/
 //btn_name = 0 - кнопка SELECT (Очистка памяти)
+//btn_name = 1 - кнопка UP
 //btn_name = 2 - кнопка RIGHT (Запись трека)
 //btn_name = 3 - кнопка LEFT (Воспроизведение трека)
+//btn_name = 4 - кнопка DOWN
 int current_btn_status(int btn_name)
 {
 	int status = 0xA;
@@ -331,6 +345,12 @@ int current_btn_status(int btn_name)
 	if (btn_name == 0)
 	{
 		status = PORT_ReadInputDataBit(MDR_PORTC, PORT_Pin_2);
+	}
+
+	//UP
+	if (btn_name == 1)
+	{
+		status = PORT_ReadInputDataBit(MDR_PORTB, PORT_Pin_5);
 	}
 
 	//RIGHT
@@ -345,17 +365,25 @@ int current_btn_status(int btn_name)
 		status = PORT_ReadInputDataBit(MDR_PORTE, PORT_Pin_3);
 	}
 
+	//DOWN
+	if (btn_name == 4)
+	{
+		status = PORT_ReadInputDataBit(MDR_PORTE, PORT_Pin_1);
+	}
+
 	//Инвертирование результата (1 - нажата, 0 - не нажата)
 	if (status == 1)
 	{
-		return 0; //не нажата
+		return 0;
 	}
 	if (status == 0)
 	{
-		return 1; //нажата
+		return 1;
 	}
-
-	return 0xA; //ошибка
+	if (status != 0 && status != 1)
+	{
+		return 0xA;
+	}
 }
 
 /*  Настройка портов ввода-вывода для кнопок  */
@@ -398,7 +426,12 @@ void BUTTONS_Init(void)
 /* Главная функция */
 int32_t main(void)
 {
-	//char track_array[1][32] = {"\xD2\xF0\xE5\xEA 1"};
+	/* Список треков */
+	char track_array[1][32] = {"\xD2\xF0\xE5\xEA 1", "\xD2\xF0\xE5\xEA 2"};
+	/* текущий трек */
+	int current_track = 0;
+	/* количество треков */
+	int num_of_tracks = 2;
 	/* определение выводимых на экран строк (кириллица) */
 	char ochistka[] = "\xCE\xF7\xE8\xF1\xF2\xEA\xE0";
 	char steret[] = "SELECT:\xD1\xF2\xE5\xF0\xE5\xF2\xFC";
@@ -434,11 +467,37 @@ int32_t main(void)
 	int current_status_select = 0; //кнопка очистка памяти
 	int current_status_right = 0;  //кнопка записи трека
 	int current_status_left = 0;   //кнопка воспроизведения трека
+	int current_status_up = 0;	 //кнопка перехода к следующему треку
+	int current_status_down = 0;   //кнопка перехода к предыдущему треку
 
 	/* Основной цикл проверки кнопок */
 	while (1)
 	{
 		/* проверка нажатия кнопок */
+
+		//UP -> Переход к предыдущему треку
+		if (current_btn_status(0) == 1 && current_status_select == 0)
+		{
+			//обработка нажатия (начало)
+			current_status_up = 1;
+			current_track += 1;
+			if (current_track == num_of_tracks)
+				current_track = 0;
+			//обработка нажатия (конец)
+		}
+		current_status_up = current_btn_status(1);
+
+		//DOWN -> переход к следующему треку
+		if (current_btn_status(0) == 1 && current_status_select == 0)
+		{
+			//обработка нажатия (начало)
+			current_status_down = 1;
+			current_track -= 1;
+			if (current_track == -1)
+				current_track = num_of_tracks - 1;
+			//обработка нажатия (конец)
+		}
+		current_status_down = current_btn_status(4);
 
 		//SELECT -> Очистка памяти
 		if (current_btn_status(0) == 1 && current_status_select == 0)
@@ -470,13 +529,16 @@ int32_t main(void)
 		if (current_btn_status(2) == 1 && current_status_right == 0)
 		{
 			//обработка нажатия (начало)
-			if (track_is_empty() == 1){
+			if (track_is_empty() == 1)
+			{
 				U_MLT_Put_String(zapis, 4);
 				U_MLT_Put_String("", 5);
 				U_MLT_Put_String("", 6);
 				U_MLT_Put_String("", 7);
 				write_track();
-			} else {
+			}
+			else
+			{
 				U_MLT_Put_String(zapisan, 4);
 				U_MLT_Put_String("", 5);
 				U_MLT_Put_String("", 6);
@@ -513,6 +575,7 @@ int32_t main(void)
 		current_status_left = current_btn_status(3);
 
 		//Вывод селект-листа треков на экран
+		U_MLT_Put_String(track_array[current_track], 3);
 		U_MLT_Put_String(zapis_short, 4);
 		U_MLT_Put_String(steret, 5);
 		U_MLT_Put_String(vosproizvedenie_short, 6);
